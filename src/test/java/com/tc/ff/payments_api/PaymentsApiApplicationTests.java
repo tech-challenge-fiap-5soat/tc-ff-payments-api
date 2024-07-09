00fa46cc3cab487fe1087478ffe3a79ae47a0f14
Reinaldo Augusto Bocchi <reinaldo.bocchi@gmail.com>
2024-07-09 10:55:08 -0400

    feat: add spring boilerplate and initial configs

---
+++ src/test/java/com/tc/ff/payments_api/PaymentsApiApplicationTests.java
@@ -0,0 +1,11 @@
+package com.tc.ff.payments_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentsApiApplicationTests {
+
+    @Test
+    void contextLoads() {}
+}
